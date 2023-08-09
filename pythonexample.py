0f6c3d83511e8d7dbd1e9b612c4fabe1df44f4b9
TechProfGit <139487183+TechProfGit@users.noreply.github.com>
2023-08-09 11:37:37 +0100

    Update pythonexample.py

--- pythonexample.py
+++ pythonexample.py
@@ -35,5 +35,23 @@ Click "Generate."
 my_variable=$(databricks notebooks run --notebook-path "<notebook-path>" --json "{\"run_my_code\": true}" | jq -r '.metadata.userExtraPythonResults.my_variable')
 echo $my_variable
 
+#---------------
+import re
 
+def replace_url_in_file(input_file, output_file, search_pattern, replace_string):
+    with open(input_file, 'r') as f:
+        content = f.read()
 
+    modified_content = re.sub(search_pattern, replace_string, content)
+
+    with open(output_file, 'w') as f:
+        f.write(modified_content)
+
+input_file_path = 'input.txt'
+output_file_path = 'output.txt'
+search_pattern = r'https://mywebsite\.com/.+?\.jpeg'
+replace_string = r'https://images.com\g<0>'
+
+replace_url_in_file(input_file_path, output_file_path, search_pattern, replace_string)
+
+print("URLs replaced successfully.")
